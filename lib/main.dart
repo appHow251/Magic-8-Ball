67e462f9960691548e068fce4517c8b7a1034ffd
apphow <apehow111@gmail.com>
2020-07-04 14:57:52 -0400

    added body: BallPage(), and BallPage StatefulWidget class

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: magic8Ball(),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text(
+            'Ask Me Anything',
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: BallPage(),
       ),
-    );
+    ),
+  );
+}
 
-class magic8Ball extends StatelessWidget {
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          'Ask Me Anything',
-          style: TextStyle(
-            color: Colors.blueGrey,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: Container(),
-    );
+    return Container();
   }
 }
